10084d6322e0f435a83d006a725c99453bf93e9c
Angel <angelfernandezblanco16@gmail.com>
2025-03-11 11:27:17 +0100

    Creo el movimiento lateral del player en el script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    public float velocidad; 
+    public float velocidadMax;
+
+    private Rigidbody2D rPlayer;
+    private float h;
+
+    private bool miraDerecha = true;
+
+    void Start(){
+        rPlayer = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Update(){
+        giraPlayer(h); 
+    }
+
+    void FixedUpdate()
     {
-        
+        h = Input.GetAxisRaw("Horizontal");
+        rPlayer.AddForce(Vector2.right * velocidad * h);
+        float limiteVel = Mathf.Clamp(rPlayer.linearVelocity.x, -velocidadMax, velocidadMax);
+        rPlayer.linearVelocity = new Vector2(limiteVel, rPlayer.linearVelocity.y);
+    }
+
+    void giraPlayer(float horizontal){
+        if ((horizontal < 0 && miraDerecha) || (horizontal > 0 && !miraDerecha))
+        {
+            miraDerecha = !miraDerecha;
+            Vector3 escalaGiro = transform.localScale;
+            escalaGiro.x *= -1;
+            transform.localScale = escalaGiro;
+        }
     }
 }
